50c86926da0c3de668ac0c4c154691fb39c2cfb7
626 <nurjigitumarov22@gmail.com>
2023-07-10 19:05:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* int a =0;
+        while (a<10){
+            System.out.println("hello world");
+            a++;
+
+        }*/
+
+//       Scanner scanner = new Scanner(System.in);
+//        System.out.println("san jazynyz");
+//        int a = scanner.nextInt();
+//
+//         int count =0;
+//        while (a>0){
+//     //   System.out.println(a);
+//        a/=10;
+//        count++;
+//        }
+//        System.out.println("bizdin san"+count+"orunduu");
+       /* int a = 4;
+        int b = 6;
+        while (a<b){
+            a=a+1;
+            System.out.println("java");
+        }*/
+//        int e=4;
+//        int f=6;
+//        while (e<f){
+//            e=f+e;
+//            System.out.println("java");
+//        }
+
+        Scanner scanner = new Scanner(System.in);
+       int a =scanner.nextInt();
+       int b = scanner.nextInt();
+        while (a<b){
+            System.out.println(Math.pow(a,2));
+            a++;
+
+
+
+        }
+      /*  Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = 0;
+        a++;
+        while (a>10){
+            System.out.println("hello"+a);
+        }*/
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = 10;
+//
+//        while (a>b){
+//            if(a%2!=0){
+//                System.out.print(a+" ");
+//            }
+//            a--;
+//        } //20дан 10го чейинки сандардын так гы
+       /* Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = 0;
+        a--;
+        while (a>b){
+            b+=2;
+            System.out.println(b);
+        }*/ //жуп сандарды чыгаруу жолуу
+
+//        Scanner scanner = new Scanner(System.in);
+//        int s = scanner.nextInt();
+//        if (s == 1){
+//            System.out.println("понедельник жаны теема!");
+//        } else if (s == 2) {
+//            System.out.println("вторник практика!");
+//        } else if (s == 3){
+//            System.out.println("среда теориялык сабак ");
+//        } else if (s == 4){
+//            System.out.println("чертверг техничиский ангилский");
+//        } else if (s ==5) {
+//            System.out.println("пятница теориялык сабак ");
+//        } else if (s ==6) {
+//            System.out.println("суббота практикалык сабак ");
+//        } else if (s == 7) {
+//            System.out.println("воскрение выходный ");
+//
+//        }else
+//       {
+//          System.out.println("туура эмес ");
+//
+//      }
+
+
+
+
+
+
+    } }
+
+
